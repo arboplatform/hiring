52c392a820f7e9dae4f56cb180adf6cbd785b382
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-23 17:43:27 -0400

    chore: starting migrations

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -2,10 +2,10 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: "database",
   port: 5432,
   username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PWD,
+  password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   synchronize: false,
   logging: true,
