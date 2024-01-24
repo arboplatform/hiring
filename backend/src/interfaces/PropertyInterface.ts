11ee66319d15f34daab8b2251e95b6d56736de7e
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-24 19:52:12 -0400

    refactor(propertyinterface): change the address type for your creation interface

--- backend/src/interfaces/PropertyInterface.ts
+++ backend/src/interfaces/PropertyInterface.ts
@@ -1,7 +1,8 @@
 import { Address } from "../entities/Address";
 import { Property } from "../entities/Property";
+import { IAddressCreate } from "./AddressInterface";
 
 export interface IPropertyCreate
   extends Pick<Property, "value" | "size" | "title"> {
-  address: Pick<Address, "city" | "number" | "state" | "street" | "zipCode">;
+  address: IAddressCreate;
 }
