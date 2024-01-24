1196088c0d27ac4290230e6bd96c49fb340aba8d
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-24 14:31:08 -0400

    feat: created authenticated user interface

--- backend/src/interfaces/UserInterface.ts
+++ backend/src/interfaces/UserInterface.ts
@@ -4,3 +4,5 @@ export interface IUserCreate
   extends Pick<User, "email" | "name" | "password"> {}
 
 export interface IUserAuth extends Pick<User, "email" | "password"> {}
+
+export interface IUserAutheticated extends Pick<User, "id" | "email"> {}
