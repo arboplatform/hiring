db566d3b8b3fdd5cdd60e9e3c1a05959e75dc89c
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-24 15:13:53 -0400

    feat: created address table

--- backend/src/entities/Property.ts
+++ backend/src/entities/Property.ts
@@ -2,16 +2,22 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
   ManyToOne,
+  OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
 import { User } from "./User";
+import { Address } from "./Address";
 
 @Entity("properties")
 export class Property {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: "varchar" })
+  title: boolean;
 
   @Column({ type: "boolean", default: false })
   sold: boolean;
@@ -30,4 +36,8 @@ export class Property {
 
   @ManyToOne(() => User, (user) => user.properties)
   user: User;
+
+  @OneToOne(() => Address)
+  @JoinColumn()
+  address: Address;
 }
