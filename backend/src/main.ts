c542298d07916379c5dc1a534ddb9ddc82552a7a
luanjunior-dev <luanjunior-dev@outlook.com>
2024-01-24 14:48:24 -0300

    docs: :memo: Add Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,10 @@ import 'reflect-metadata';
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'js-yaml';
+import fs from 'fs';
+import path from 'path';
 import { propertyRoutes } from './routes/';
 import mongoConnect from './config/database/mongoConfig';
 
@@ -21,6 +25,14 @@ app.use(cors());
 
 // Middleware Json
 app.use(express.json());
+
+// Carregando o arquivo YAML
+const swaggerDocument = yaml.load(
+  fs.readFileSync(path.join(__dirname, 'swaggerConfig.yaml'), 'utf8'),
+);
+
+// Rota para a documentação Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.send('Hello! :3');
