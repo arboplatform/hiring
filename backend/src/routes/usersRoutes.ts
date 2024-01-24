9d6848a6f27274fcb59fae4a33d5b00c6a05a586
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-24 19:11:29 -0400

    feat(usersroutes.ts): add validation middleware to the user creation route

--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -1,12 +1,18 @@
 import { Router, Express } from "express";
 import { UsersController } from "../controllers/UsersController";
 import { isAuthenticatedMiddleware } from "../middlewares";
+import { validationMiddleware } from "../middlewares/validationMiddleware";
+import { createUserValidate } from "../validations/user";
 
 const usersRoutes = (app: Express) => {
   const router = Router();
   const controller = new UsersController();
 
-  router.post("", controller.create.bind(controller));
+  router.post(
+    "",
+    validationMiddleware(createUserValidate),
+    controller.create.bind(controller)
+  );
   router.get("", controller.findAll.bind(controller));
   router.post("/auth", controller.auth.bind(controller));
   router.post(
