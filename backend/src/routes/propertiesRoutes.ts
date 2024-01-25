e53171efc87b83273a3f00b014b59a3beaf13844
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-24 20:43:36 -0400

    feat(propertiesroutes): adds validation middleware to create property

--- backend/src/routes/propertiesRoutes.ts
+++ backend/src/routes/propertiesRoutes.ts
@@ -1,6 +1,8 @@
 import { Express, Router } from "express";
 import { PropertiesController } from "../controllers/PropertiesController";
 import { isAuthenticatedMiddleware } from "../middlewares";
+import { validationMiddleware } from "../middlewares/validationMiddleware";
+import { createPropertyValidate } from "../validations/property";
 
 const propertiesRoutes = (app: Express) => {
   const router = Router();
@@ -9,6 +11,7 @@ const propertiesRoutes = (app: Express) => {
   router.post(
     "",
     isAuthenticatedMiddleware({ required: true }),
+    validationMiddleware(createPropertyValidate),
     controller.create.bind(controller)
   );
 
