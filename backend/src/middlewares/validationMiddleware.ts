444429c83a312a78a2096ff37217b968d74e340f
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-25 14:46:48 -0400

    feat(validationmiddleware.ts): adding new typing

--- backend/src/middlewares/validationMiddleware.ts
+++ backend/src/middlewares/validationMiddleware.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from "express";
-import { ZodOptional, AnyZodObject } from "zod";
+import { ZodOptional, AnyZodObject, ZodEffects } from "zod";
 
 export const validationMiddleware =
-  (validate: AnyZodObject | ZodOptional<any>) =>
+  (validate: AnyZodObject | ZodOptional<any> | ZodEffects<any>) =>
   (req: Request, _res: Response, next: NextFunction) => {
     const parse = validate.parse(req.body);
 
