0f71c174308a3d1f197c25b66ac8be379d9642b7
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-25 15:41:16 -0400

    feat: initial react query configuration

---
+++ frontend/src/globalStyles.ts
@@ -0,0 +1,5 @@
+import { Interpolation, Theme } from "@mui/material/styles";
+
+export const globalStyles: Interpolation<Theme> = {
+  body: { backgroundColor: "#00A9FF" },
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,13 +7,27 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { GlobalStyles, ThemeProvider } from "@mui/material";
+import { Toaster } from "react-hot-toast";
 import theme from "./theme";
+import { globalStyles } from "./globalStyles";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles styles={{ body: { backgroundColor: "#00A9FF" } }} />
-      <RouterProvider router={router} />
+      <GlobalStyles styles={globalStyles} />
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        toastOptions={{
+          duration: 5000,
+        }}
+      />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
