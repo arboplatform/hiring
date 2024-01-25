6171319fee505ddcdb6b5d58780fe0b167955df4
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-25 12:29:45 -0400

    feat: add a default color to the body

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,12 +6,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { ThemeProvider } from "@mui/material";
+import { GlobalStyles, ThemeProvider } from "@mui/material";
 import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles styles={{ body: { backgroundColor: "#00A9FF" } }} />
       <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
