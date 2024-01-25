ce71db67262cb8076dca38b54504d69548c8b16a
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-25 16:29:43 -0400

    feat: add color settings to the back

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,15 @@ import express from "express";
 import { errorMiddleware } from "./middlewares";
 import { appRoutes } from "./routes";
 import "express-async-errors";
+import cors, { CorsOptions } from "cors";
 
+const corsOption: CorsOptions = {
+  origin: "*",
+};
 const app = express();
 
 app.use(express.json());
+app.use(cors(corsOption));
 
 appRoutes(app);
 
