53432469f886f70199730d83a7ca33e3c66879cb
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-24 23:56:35 -0400

    feat: create meterial ui theme configuration

---
+++ frontend/src/theme.tsx
@@ -0,0 +1,24 @@
+import { createTheme } from "@mui/material/styles";
+import { red, green } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#00A9FF",
+    },
+    secondary: {
+      main: "#89CFF3",
+    },
+    error: {
+      main: red.A100,
+    },
+    success: {
+      main: green.A200,
+    },
+  },
+  typography: {
+    fontFamily: '"Poppins", sans-serif',
+  },
+});
+
+export default theme;
