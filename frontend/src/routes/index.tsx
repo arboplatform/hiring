bb71b68a9cf7905da094dc881847a2d341260177
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-25 20:17:58 -0400

    feat: adds protection route for home

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,11 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { Home } from "../pages/Home";
+import { ProtectedRoute } from "./ProtectedRoute";
+import React from "react";
+import { Loading } from "../components/Loading";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: (
+      <ProtectedRoute>
+        <React.Suspense fallback={<Loading />}>
+          <Home />
+        </React.Suspense>
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/login",
