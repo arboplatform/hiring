c72de948882784da006d819aa3f8d6e48f22bbed
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-25 23:41:09 -0400

    feat: adds the functionality to create properties on the home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,52 @@
-import { Typography } from "@mui/material";
+import AddHomeIcon from "@mui/icons-material/AddHome";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { Navbar } from "../components/Navbar";
+import { useState } from "react";
+import { DialogCreateProperty } from "../components/DialogCreateProperty";
 
 const Home = () => {
-  return <Typography variant="h1">Home</Typography>;
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpenDialog = () => setOpen(true);
+  const handleClickCloseDialog = () => setOpen(false);
+
+  return (
+    <Stack>
+      <Navbar />
+      <DialogCreateProperty open={open} handleClose={handleClickCloseDialog} />
+      <Stack>
+        <Box
+          maxWidth="lg"
+          width="100%"
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            margin: "0 auto",
+            paddingY: 3,
+            color: "white",
+          }}
+        >
+          <Typography component="h4" variant="h5" fontWeight="500">
+            Lista de imóveis
+          </Typography>
+
+          <Button
+            variant="contained"
+            color="success"
+            sx={{
+              color: "white",
+            }}
+            endIcon={<AddHomeIcon />}
+            onClick={handleClickOpenDialog}
+          >
+            Adicionar imóvel
+          </Button>
+        </Box>
+      </Stack>
+    </Stack>
+  );
 };
 
 export { Home };
