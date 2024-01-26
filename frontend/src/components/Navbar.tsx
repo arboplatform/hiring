480972487bde0221f719e1105d6ccb049a3f826f
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-25 23:19:20 -0400

    feat: create the Navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { clearToken } from "../lib/localStorage";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const handleClickLogout = () => {
+    clearToken();
+    navigate("/login");
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "white",
+          paddingY: "10px",
+        }}
+      >
+        <Toolbar
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            maxWidth: "1200px",
+            width: "100%",
+            margin: "0 auto",
+          }}
+        >
+          <Box>
+            <Typography variant="h4" component="h2" color="gray">
+              Imovel
+            </Typography>
+          </Box>
+          <Button
+            variant="contained"
+            sx={{ color: "white" }}
+            onClick={handleClickLogout}
+          >
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export { Navbar };
