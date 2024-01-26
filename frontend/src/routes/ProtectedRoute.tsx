cf181d3f3684512b80aeb3fcfe6479fa13e93497
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-25 20:14:13 -0400

    feat: creates component to protect authenticated route

---
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,21 @@
+import { Navigate } from "react-router-dom";
+import { useToken } from "../hooks/useToken";
+import { Loading } from "../components/Loading";
+
+type ProtectedRouteProps = {
+  children: React.ReactNode;
+};
+
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const { data, isLoading } = useToken();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!data) return <Navigate to="/login" replace />;
+
+  return <>{children}</>;
+};
+
+export { ProtectedRoute };
