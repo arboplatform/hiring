5bba51f40317596ed995518e38704af7f6ae5bef
Guilherme Farrel de Souza <garcia.farrel@gmail.com>
2024-02-17 08:53:53 -0300

    feat(frontend): :mag: added Metadata

--- frontend/src/app/estates/[slug]/page.tsx
+++ frontend/src/app/estates/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { Metadata } from 'next';
+
 import { getEstateBySlug, updateEstate } from '@src/app/actions';
 import { Form } from '@src/components/Form';
 import { EstateDTO } from '@src/types/entities/estate';
@@ -6,6 +8,17 @@ import { transformEstateIntoDTO } from '@src/utils/transformEstateIntoDTO';
 type Props = {
   params: {
     slug: string;
+  };
+};
+
+export const generateMetadata = async ({
+  params: { slug },
+}: Props): Promise<Metadata> => {
+  const estate = await getEstateBySlug(slug);
+
+  return {
+    title: estate.name,
+    description: estate.description,
   };
 };
 
--- frontend/src/app/estates/new/page.tsx
+++ frontend/src/app/estates/new/page.tsx
@@ -1,6 +1,13 @@
+import { Metadata } from 'next';
+
 import { createEstate } from '@src/app/actions';
 import { Form } from '@src/components/Form';
 import { EstateDTO } from '@src/types/entities/estate';
+
+export const metadata: Metadata = {
+  title: 'Novo imóvel',
+  description: 'Crie um novo imóvel',
+};
 
 export default async function Page() {
   const submit = async (estate: EstateDTO) => {
